7acda4fd8c5bf9aee64253a032cf6f5399b6f50b
Jackson Chen <a@jackson.tw>
2021-03-11 00:29:59 +0900

    🚀 Add hello world with docker

---
+++ 1-hello-world/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public void run(String[] args) {
+        System.out.println("Hello world");
+    }
+
+    public static void main(String[] args) {
+        new Main().run(args);
+    }
+}
---
+++ 1-hello-world/README.md
@@ -0,0 +1,13 @@
+# Start 🌟
+
+if you haven't get openjdk
+```
+docker pull openjdk
+```
+
+
+# Run 🚀 
+```
+docker build -t my-java-app .
+docker run -it --rm --name my-running-app my-java-app
+```
